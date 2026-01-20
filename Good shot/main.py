ce2251a7e1cea85d72691eb8ff5a1844f00931a8
Aaravcodespython <sheeta08@googlemail.com>
2026-01-20 16:53:18 +0000

    Update main.py

--- Good shot/main.py
+++ Good shot/main.py
@@ -11,17 +11,42 @@ WIDTH = 1000
 HEIGHT = 600
 #the constants dont modify while the code is running
 
+#creating variables within the game
+score = 0
+message = "Catch the fly!"
+
 #fly (game character/actor)
 fly = Actor("cartoon_fly")
 
 #positioning the actor at a random position
 fly.x = random.randint(50,950)
 fly.y = random.randint(50,550)
+
 #draw the screen using a system function
 def draw():
-    screen.fill("green")
+    screen.fill("orange")
+    
     #displaying actor on the screen
     fly.draw()
-#instruction to keep the screen running
+    
+    #displaying text on the screen
+    screen.draw.text(message,center = (WIDTH/2,50),color = "black",fontsize = 40)
+    screen.draw.text(f"Score: {score}",topright = (900,40),color = "black",fontsize = 40)
 
+#detects when the user clicks and where
+def on_mouse_down(pos):
+    #using global variable message
+    global message, score
+
+    #checking if clicked on fly
+    if fly.collidepoint(pos):
+        fly.x = random.randint(50,950)
+        fly.y = random.randint(50,550)
+        message = "You got me!"
+        score += 1
+    
+    else:
+        message = "Good luck next time!"
+
+#instruction to keep the screen running
 pgzrun.go()
